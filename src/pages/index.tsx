7d99329e9c9576087d53607f53220782cf2f7299
douglasblnk <douglasblank1@gmail.com>
2021-02-25 22:18:01 -0300

    feat: imported new components in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,31 @@
 import styles from '../styles/pages/Home.module.scss';
 
+import Head from 'next/head';
+
+import { CompletedChallenges } from '../components/CompletedChallenges/CompletedChallenges';
 import { ExperienceBar } from '../components/ExperienceBar/ExperienceBar';
 import { Profile } from '../components/Profile/Profile';
+import { Countdown } from '../components/Countdown/Countdown';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>
+          Início | Focus.ing
+        </title>
+      </Head>
+      
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
 
         <div>
-
         </div>
       </section>
     </div>
