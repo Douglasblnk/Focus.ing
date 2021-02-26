55828eb3fa66b70c16dedae699ed961a5c6e9572
douglasblnk <douglasblank1@gmail.com>
2021-02-25 22:17:35 -0300

    feat: implemented completedChallenge component

---
+++ src/components/CompletedChallenges/CompletedChallenges.tsx
@@ -0,0 +1,11 @@
+import styles from './CompletedChallenges.module.scss';
+
+export const CompletedChallenges = () => (
+  <div className={styles.completedChallengesContainer}>
+    <span>
+      Desafios completos
+    </span>
+
+    <span>5</span>
+  </div>
+);
